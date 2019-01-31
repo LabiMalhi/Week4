1028e10e2d5c898ddc54670e4237674101d7ed2c
visualstudio <visualstudio@230-PC43>
2019-01-31 12:01:16 -0500

    callin function with onload event

--- Week49/js/main.js
+++ Week49/js/main.js
@@ -1,1 +1,5 @@
 ﻿// Your code here!
+function myfunction() { 
+    var cat = new Data();
+    document.body.innerHTML = " <h1> The date is " + cat.getMonth() + "/" + cat.getDate() + "/" + cat.getFullYear() + " </h1> "
+}
